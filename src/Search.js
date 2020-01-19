2a1f28437a9fbc616645d272f3b5aa8cf924dcb0
RyoOkamto <r.okamoto@sonicmoov.com>
2020-01-19 16:31:43 +0900

    Fix errors

--- src/Search.js
+++ src/Search.js
@@ -19,14 +19,15 @@ const Search = (props) => {
   }
 
   return (
-    <form className="search">
-      <input
-        value={searchValue}
-        onChange={handleSearchInputChanges}
-        type="text"
+      <form className="search">
+        <input
+          value={searchValue}
+          onChange={handleSearchInputChanges}
+          type="text"
         />
-      <input onClick={callSearchFunction}>
-  );
+        <input onClick={callSearchFunction} type="submit" value="SEARCH" />
+      </form>
+    );
 }
 
 export default Search;
